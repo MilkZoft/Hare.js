539f9f3df7b119633de44197f10cae250a95cf67
Carlos <carlos@milkzoft.com>
2014-02-28 13:35:19 -0800

    Translations with nodes.

--- languages/en.js
+++ languages/en.js
@@ -1,4 +1,17 @@
 var en = {
+    errors: {
+        '404': 'Error 404: Page not found',
+        '500': 'Error 500: Internal Server Error'
+    },
+
+    users: {
+        'username': 'Username'
+    },
+
+    global: {
+        'hi': 'Hi'
+    },
+
 	'hello.world': 'Hello World',
     'welcome.world': 'Welcome World'
 };
--- languages/es.js
+++ languages/es.js
@@ -1,4 +1,17 @@
 var es = {
+	errors: {
+        '404': 'Error 404: Página no encontrada',
+        '500': 'Error 500: Error interno del servidor'
+    },
+
+    users: {
+        'username': 'Nombre de usuario'
+    },
+
+    global: {
+        'hi': 'Hola'
+    },
+
 	'hello.world': 'Hola Mundo',
     'welcome.world': 'Bienvenido Mundo'
 };
