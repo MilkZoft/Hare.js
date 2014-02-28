8dd6678dc57431f1e2056419b816cb0782060cbc
Carlos <carlos@milkzoft.com>
2014-02-28 12:16:29 -0800

    Using translations

--- controllers/users.js
+++ controllers/users.js
@@ -5,12 +5,12 @@ var globals = require('../config/config'),
 	db = require('../db/mysql');
 
 module.exports = {
-    index: function(req, res, params)
+    index: function(req, res, params, lang)
     { 
-    	res.send('Index');
+    	res.send(lang.translate['hello.world']);
     },
 
-    get: function(req, res, params)
+    get: function(req, res, params, lang)
     {
     	db.find({id: 1, table: 'users', fields: '*'}, function(error, data) {
     		res.render('users_user', { users: data });
