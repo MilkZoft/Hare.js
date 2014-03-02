6f7d1ae10573a13a521203edc1c914ace2adeab8
Carlos Santana <carlos@milkzoft.com>
2014-03-01 23:13:15 -0800

    Refactoring...

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// Global variables for templates
+// Global variables
 global.config = require('./config/config');
 global.debug = require('./system/helpers/debug');
 global.i18n = require('./system/helpers/i18n');
@@ -45,7 +45,7 @@ app.use(function(req, res, next) {
   global.lang = require('./languages/' + global.config.site.language);
 
   var path = req.url.replace(/^\/|\/$/g, ''),
-      url  = (path) ? path.split('/') : false,
+      url = (path) ? path.split('/') : false,
       params = [];
 
   // Default controller is defined in global.config.application.controllers.default
@@ -54,30 +54,31 @@ app.use(function(req, res, next) {
 
     execute.index(req, res, [], lang);
   } else {
-    var controller = (global.i18n.isLanguage(url[0])) ? url[1] : url[0];
-
-    if (global.i18n.isLanguage(url[0]) && url.length == 1) {
-      global.lang = require('./languages/' + url[0]);
-
-      var execute = require('./controllers/' + global.config.application.controllers.default);
-
-      execute.index(req, res, []);
-    } else if (global.i18n.isLanguage(url[0])) {
+    if (global.i18n.isLanguage(url[0])) {
+      var controller = url[1];
       var action = (url[2]) ? url[2] : 'index';
 
       global.lang = require('./languages/' + url[0]);
-    } else  {
-      var action = (url[1]) ? url[1] : 'index';
-    }
-      
-    if (url.length > 2) {
-      var j = (url.length > 3 && global.i18n.isLanguage(url[0])) ? 3 : 2;
 
+      if (url.length == 1) {
+        var execute = require('./controllers/' + global.config.application.controllers.default);
+
+        execute.index(req, res, []);
+      }
+
+      var j = (url.length > 3) ? 3 : 2;
+    } else {
+      var controller = url[0];
+      var action = (url[1]) ? url[1] : 'index';
+      var j = 2;
+    }
+
+    if (url.length > 2) {
       for (var i = j; i <= url.length; i++) {
         if (url[i]) {
           params.push(global.str.sanitize(url[i]));
         }
-      }   
+      }
     }
 
     try {
@@ -85,7 +86,8 @@ app.use(function(req, res, next) {
 
       execute[action](req, res, params);
     } catch(e) {
-      console.log("ERROR", e);
+      console.log("---ERROR FATAL---", e);
+
       var execute = require('./controllers/error');
 
       execute.error404(req, res, []);
@@ -93,6 +95,6 @@ app.use(function(req, res, next) {
   }
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Running on port ' + app.get('port'));
 });
