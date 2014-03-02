34448cc198309a6b171ee6a58203ffc206e21212
Carlos Santana <carlos@milkzoft.com>
2014-03-01 19:17:59 -0800

    System model

---
+++ system/model.js
@@ -0,0 +1,27 @@
+var db = require('../db/mysql');
+
+module.exports = function(schema) {
+  return {
+    get: function(query, callback) {
+      if (!schema.table) {
+        return false;
+      }
+
+      if (!isNaN(query)) {
+        schema.key = (schema.key) ? schema.key : 'id';
+        schema.fields = (schema.fields) ? schema.fields : '*';
+        
+        db.find({
+          id: parseInt(query), 
+          table: schema.table, 
+          fields: schema.fields, 
+          key: schema.key
+        }, callback);
+      } else {
+        
+      }
+
+      return false;
+    }
+  };
+}
