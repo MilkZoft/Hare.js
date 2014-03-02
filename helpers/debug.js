0ea8972e6fa75dc46fcf31c7ba2d71f88f161709
Carlos Santana <carlos@milkzoft.com>
2014-03-01 19:17:18 -0800

    Debug with now display SQL queries

--- helpers/debug.js
+++ helpers/debug.js
@@ -3,10 +3,15 @@ var self = {
   {
     var debugLines = (options.l) ? + options.l : '';
     var debugFunction = (options.f) ? 'function:' + options.f : null;
-    var debugParams = (options.p) ? options.p : null;
-    var debugReturn = (options.r) ? '|Return: ' + options.r : '';
-    var debugMessage = options.file + ':' + options.l + '|' + debugFunction + '(' + debugParams + ')' + debugReturn;
 
+    if (options.s) {
+      var debugMessage = options.file + ':' + options.l + '|' + debugFunction + '|Sql:' + options.s;
+    } else {
+      var debugParams = (options.p) ? options.p : null;
+      var debugReturn = (options.r) ? '|Return: ' + options.r : '';
+      var debugMessage = options.file + ':' + options.l + '|' + debugFunction + '(' + debugParams + ')' + debugReturn; 
+    }
+     
     if (global.config.server.debug) {
       console.log(debugMessage);
     }
