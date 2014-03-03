6d41e1f1904d46104410ae1d9e2062e3b26b6248
Carlos Santana <carlos@milkzoft.com>
2014-03-02 18:42:50 -0800

    MySQL ActiveRecord fixed

--- system/db/mysql.js
+++ system/db/mysql.js
@@ -7,26 +7,83 @@ var connection = mysql.createConnection({
   host: global.config.db.host,
   user: global.config.db.user,
   password: global.config.db.password,
-  database: global.config.db.database
+  database: global.config.db.database,
+  debug: global.config.db.debug
 });
 
 module.exports = {
-  find: function (options, callback)
+  find: function (obj, callback)
   { 
-    if (!options.id || !options.table) {
+    if (!obj.id || !obj.table) {
       return false;
     }
 
-    var fields = (options.fields) ? options.fields : '*',
-        sql = 'SELECT ' + fields + ' FROM ' + options.table + ' WHERE ' + options.key + ' = ' + options.id;
+    var fields = (obj.fields) ? obj.fields : '*',
+        sql  = 'SELECT ' + fields        + ' ';
+        sql += 'FROM '   + obj.table + ' ';
+        sql += 'WHERE '  + obj.key   + ' = ' + obj.id;
 
     global.debug.set({ 
       file: 'db/mysql.js', 
-      l: 23, 
+      l: 25, 
       f: 'find', 
       s: sql
     });
 
     connection.query(sql, callback);
-  }
+  },
+
+  findBy: function (obj, callback)
+  { 
+    if (!obj.table) {
+      return false;
+    }
+
+    var fields = (obj.fields) ? obj.fields : '*',
+        group  = (obj.group) ? ' GROUP BY ' + obj.group + ' ' : '',
+        order  = (obj.order) ? ' ORDER BY ' + obj.order + ' ' : '',
+        limit  = (obj.limit) ? ' LIMIT ' + obj.limit + ' ' : '',        
+        sql    = 'SELECT ' + fields    + ' ';
+        sql   += 'FROM '   + obj.table + ' '; 
+        sql   += 'WHERE '  + obj.field + ' = \'' + obj.value + '\'';
+        sql   += group,
+        sql   += order,
+        sql   += limit;
+
+    global.debug.set({ 
+      file: 'db/mysql.js', 
+      l: 70, 
+      f: 'findBy', 
+      s: sql
+    });
+
+    connection.query(sql, callback);
+  },
+
+  findBySQL: function (obj, callback)
+  { 
+    if (!obj.table || !obj.query) {
+      return false;
+    }
+
+    var fields = (obj.fields) ? obj.fields : '*',
+        group  = (obj.group) ? ' GROUP BY ' + obj.group + ' ' : '',
+        order  = (obj.order) ? ' ORDER BY ' + obj.order + ' ' : '',
+        limit  = (obj.limit) ? ' LIMIT ' + obj.limit + ' ' : '',        
+        sql    = 'SELECT ' + fields    + ' ';
+        sql   += 'FROM '   + obj.table + ' '; 
+        sql   += 'WHERE '  + obj.query + ' ';
+        sql   += group,
+        sql   += order,
+        sql   += limit;
+
+    global.debug.set({ 
+      file: 'db/mysql.js', 
+      l: 82, 
+      f: 'findBySQL', 
+      s: sql
+    });
+
+    connection.query(sql, callback);
+  },  
 };
