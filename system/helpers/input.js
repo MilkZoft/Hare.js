98d6a750a897e70e09e020c08f17bad75f1ab0ca
Carlos Santana <carlos@milkzoft.com>
2014-03-04 21:41:15 -0800

    get, has, isGet and isPost added to helper input

--- system/helpers/input.js
+++ system/helpers/input.js
@@ -1,10 +1,24 @@
 var self = {
   get: function(field) 
   {       
-    console.log(field);
+    return (global.req.body[field]) ? global.req.body[field] : false;
+  },
+
+  has: function(field) 
+  {       
+    return (global.req.body[field]) ? true : false;
+  },
+
+  isGet: function()
+  {
+    return (global.req.method == 'GET') ? true : false;
+  },
+
+  isPost: function()
+  {
+    return (global.req.method == 'POST') ? true : false;
   }
 
-  
 };
 
 module.exports = self;
